34070b15ad4b0ab0ac2f71ce784ba298214a5069
Jordan Mozebo <73420348+Jmozebo93@users.noreply.github.com>
2023-08-29 23:10:59 -0400

    Update

--- senior_project/Login.php
+++ senior_project/Login.php
@@ -20,28 +20,43 @@ if ($conn->connect_error) {
 }
 
 // Get user input
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = $_POST['user_email'];
+$password = $_POST['user_password'];
+$userType = $_POST['user_type'];
 
-// Retrieve hashed password from the database
-$sql = "SELECT id, username, password_hash FROM users WHERE username = ?";
+if($userType == 'STUDENT'){
+    $sql = "SELECT StudentID, StudentEmail, StudentPassword FROM STUDENT WHERE StudentEmail = ?";
+} elseif($userType == 'ADVISOR'){
+    $sql = "SELECT AdvisorID, AdvisorEmail, AdvisorPassword FROM ADVISOR WHERE AdvisorEmail = ?";
+}
+
+
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("s", $username);
 $stmt->execute();
-$stmt->bind_result($userId, $dbUsername, $dbPasswordHash);
+if ($userType == 'STUDENT') {
+    $stmt->bind_result($StudentID, $StudentEmail, $StudentPassword);
+} elseif ($userType == 'ADVISOR') {
+    $stmt->bind_result($AdvisorID, $AdvisorEmail, $AdvisorPassword);  // Use the appropriate column aliases here
+}
+
 $stmt->fetch();
 
 // Verify password
-if (password_verify($password, $dbPasswordHash)) {
-    // Password is correct
-    $_SESSION['user_id'] = $userId;
-    $_SESSION['username'] = $dbUsername;
+if ($userType == 'STUDENT' && password_verify($password, $StudentPassword)) {
+    // Password is correct for a student
+    $_SESSION['user_id'] = $StudentID;
+    $_SESSION['username'] = $StudentEmail;
+    header("Location: dashboard.php"); // Redirect to dashboard or another page
+} elseif ($userType == 'ADVISOR' && password_verify($password, $AdvisorPassword)) {
+    // Password is correct for an advisor
+    $_SESSION['user_id'] = $AdvisorID;
+    $_SESSION['username'] = $AdvisorEmail;
     header("Location: dashboard.php"); // Redirect to dashboard or another page
 } else {
     // Invalid credentials
     header("Location: login.php?error=1"); // Redirect with error
 }
-
 $stmt->close();
 $conn->close();
 ?>
