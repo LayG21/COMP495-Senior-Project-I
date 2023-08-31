3d5ee4feead3fd8b7a40b123bb0b6e1b0ee80247
Jordan Mozebo <73420348+Jmozebo93@users.noreply.github.com>
2023-08-31 09:03:04 -0400

    Testing the connection to the database

---
+++ senior_project/Log.php
@@ -0,0 +1,7 @@
+<?php
+$conection = mysqli_connect("localhost","root","jtmo9657","STUDENT_ADVISOR_WEBAPP");
+if(!$conection)
+  die("Could not connect to the database".mysqli_connect_error());
+else
+ echo "connection established";
+?>
