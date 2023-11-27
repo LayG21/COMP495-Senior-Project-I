ccf51cd6791f14a57c9806146ae4f71706b52eec
LayG21 <ladylynai15@gmail.com>
2023-11-26 21:30:56 -0500

    Added schema for classselection

--- senior-project/backend/models/ClassSelections.js
+++ senior-project/backend/models/ClassSelections.js
@@ -4,7 +4,39 @@ const mongoose = require("mongoose");
 //not sure what to do about that
 
 const classselectionSchema = new mongoose.Schema({
+    SelectionID: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
 
+    StuID: {
+        type: Number,
+        ref: 'Student',
+        required: true,
+
+    },
+    SelectedClassCodeName: {
+        type: String,
+        required: true,
+    },
+    SelectedClassID: {
+        type: Number,
+        required: true,
+    },
+    SelectedClassName: {
+        type: String,
+        required: true,
+    },
+    Semester: {
+        type: String,
+        required: true,
+    },
+    SelectedDate: {
+        type: Date,
+        default: Date.now(),
+        required: true,
+    }
 });
 
 const ClassSelections = mongoose.model('ClassSelections', classselectionSchema);
